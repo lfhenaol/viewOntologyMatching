836a6ea3c992bc7c47ccb6f184d711e5449b8dd5
Luis Felipe Henao López <lfhenaolopez@misena.edu.co>
2016-04-30 15:20:35 -0500

    Initial commit

---
+++ Search/app/Http/routes.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    Form::macro('materializeSubmit',function($value){
+        echo
+            '<button class="btn waves-effect waves-light" type="submit" name="action">
+                '.$value.'<i class="material-icons right">send</i>
+            </button>';
+    });
+    return view('ViewOntologyMatching');
+});
